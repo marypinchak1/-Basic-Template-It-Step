b1c946fe936523387c5ce1cba1b943eddfdc5177
Mariia Pinchak <92802080+marypinchak1@users.noreply.github.com>
2022-12-04 14:34:05 +0200

    add-lesson

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,18 @@
 let slider = $(document).ready(function () {
-  $('.your-class').slick({
+  $(".your-class").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     dots: true,
-    infinite: true
+    infinite: true,
   });
 });
 
+$('.item').on('click', function () {
+    $('.item').removeClass('active');
+    $(this).addClass("active");
+    let tabs = $(this).data("tabs");
+    $(".content").addClass("d-none");
+    $(".content[data-tabs = " + tabs + "]").removeClass("d-none");
+    $(".content[data-tabs = " + tabs + "]").addClass("d-block");
+  });
