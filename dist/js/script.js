ad856bdd02fcdfdc0387fec171a133be1b8d4d1f
Mariia Pinchak <92802080+marypinchak1@users.noreply.github.com>
2022-12-11 14:59:53 +0200

    add-changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,16 @@
 let slider = $(document).ready(function () {
-  $('.your-class').slick({
+  $('.slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
-    dots: true,
-    infinite: true
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow:
+      "<img class='a-left control-c prev arrow-prev' src='../img/img/slideer-arrow-left.svg'>",
+    nextArrow:
+      "<img class='a-right control-c next arrow-next' src='../img/img/slider-arrow-right.svg'>",
   });
 });
 
